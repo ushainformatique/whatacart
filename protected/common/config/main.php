de49dd49d84a0715a046287d2d3af9fb49c5fa9c
Mayank Singhai <mayank.singhai@ushainformatique.com>
2018-12-07 16:28:07 +0530

    Fix version

--- protected/common/config/main.php
+++ protected/common/config/main.php
@@ -9,7 +9,7 @@ return ArrayHelper::merge(
                                             'vendorPath'    => VENDOR_PATH,
                                             'installed'     => $installed,
                                             'name'          => $siteName,
-                                            'version'       => '2.0.3',
+                                            'version'       => '2.0.4',
                                             'poweredByName' => 'WhatACart',
                                             'poweredByUrl'  => 'http://whatacart.com',
                                             'environment'   => $environment,
