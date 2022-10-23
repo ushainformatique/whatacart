c065b77c954485ccac3894a40c9e5ca0ba1a6217
Mayank Singhai <rajusinghai80@gmail.com>
2022-10-23 15:11:11 +0530

    Fix the version number

--- protected/common/config/main.php
+++ protected/common/config/main.php
@@ -9,7 +9,7 @@ return ArrayHelper::merge(
                                             'vendorPath'    => VENDOR_PATH,
                                             'installed'     => $installed,
                                             'name'          => $siteName,
-                                            'version'       => '2.0.6',
+                                            'version'       => '2.0.7',
                                             'poweredByName' => 'WhatACart',
                                             'poweredByUrl'  => 'http://whatacart.com',
                                             'environment'   => $environment,
